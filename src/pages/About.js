c7ca163d2066e323cccb1dd4ed19e2086fea47f5
Pelago <domingo.pelago@accenture.com>
2022-04-12 14:30:11 +0800

    Add Other Statif Pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,53 @@
 const About = () => (
-    <h1>Example About</h1>
-)
+  <div className="w-full mx-auto text-center container px-24 mt-4">
+    <h1 className="font-bold text-gray-800 uppercase hover:text-gray-700 text-5xl mb-4">
+      About Us
+    </h1>
+    <p className="mb-10">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed
+      lobortis neque. Mauris quam odio, iaculis a cursus at, dignissim ut massa.
+      Aliquam dignissim dapibus lacus, a imperdiet sem ultrices in. Sed placerat
+      laoreet sem ut commodo. Fusce vulputate sapien vel posuere tincidunt.
+      Interdum et malesuada fames ac ante ipsum primis in faucibus. Morbi
+      posuere libero non augue luctus egestas. Proin feugiat justo vitae mauris
+      viverra, lacinia placerat urna gravida. Cras auctor elit eu metus
+      convallis, eget faucibus augue finibus. Etiam ut auctor erat. Aliquam erat
+      volutpat. Donec dapibus auctor ex ut gravida. Quisque aliquam volutpat
+      sollicitudin. Pellentesque aliquam leo pulvinar lorem malesuada sagittis.
+      Ut velit tellus, pharetra sed nulla ac, placerat interdum justo. Curabitur
+      neque enim, congue at molestie id, lacinia eu ligula. Praesent pharetra
+      ligula pharetra elit vulputate finibus. Nullam quis massa sit amet felis
+      aliquam pulvinar. Pellentesque quam lacus, accumsan eu iaculis id, feugiat
+      in quam. Mauris tristique, massa vel cursus tempor, tellus sapien pretium
+      ex, et eleifend purus elit vel ante. Vivamus ac aliquet diam, at placerat
+      nisi. Ut eu massa pharetra, ultrices nunc nec, aliquam nisi. Sed ante
+      turpis, facilisis quis hendrerit accumsan, tristique a tortor. Integer ut
+      ornare lacus. Donec id dui at ipsum convallis aliquam. Suspendisse
+      venenatis sed quam sed tristique. Maecenas ornare consequat sapien, vel
+      euismod diam commodo sed. Proin eleifend eu quam sed placerat. Aenean
+      interdum placerat tellus vel consectetur. Proin sed libero purus. Quisque
+      volutpat, nisi nec pulvinar pellentesque, lorem sapien consequat leo, non
+      tristique arcu elit vitae tellus. Nunc at condimentum felis. Cras et
+      vulputate dui. In at sapien eget turpis ullamcorper mollis. Cras a orci
+      nunc. Pellentesque iaculis faucibus dolor id sollicitudin. Fusce et ante
+      lobortis lorem vehicula porta. Quisque pulvinar commodo aliquam. Curabitur
+      tincidunt convallis dapibus. Mauris quis vulputate ipsum. Praesent sed
+      ipsum et enim posuere sagittis quis ac nibh. Vivamus lobortis blandit mi
+      mollis consectetur. Suspendisse vitae ante id elit elementum finibus.
+      Suspendisse ut viverra lorem, ac aliquam turpis. Donec consequat arcu ut
+      erat vulputate volutpat. Vestibulum sit amet quam ut risus imperdiet
+      interdum. Mauris eleifend, magna dignissim molestie accumsan, mauris erat
+      vehicula sem, sed sollicitudin arcu metus non nulla. Sed ultricies lectus
+      at leo sodales, sed efficitur orci volutpat. Aliquam rhoncus pretium
+      tincidunt. Curabitur sapien est, faucibus in tortor pretium, posuere
+      convallis lectus. Mauris tempor enim commodo augue placerat, vitae
+      fringilla dui volutpat. Aliquam vel enim tellus. Cras eu libero in libero
+      pellentesque facilisis et sit amet libero. Maecenas semper pellentesque
+      nulla, eu gravida dolor ultricies sit amet. Proin vitae libero lacus.
+      Nulla pharetra dolor eros, ac semper enim commodo in. Fusce nec posuere
+      lorem.
+    </p>
+  </div>
+);
 
-export default About
+export default About;
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,12 +1,14 @@
-import { Routes, Route } from "react-router-dom"
-import Blog from '../pages/Blog'
-import About from "../pages/About"
+import { Routes, Route } from "react-router-dom";
+import Blog from "../pages/Blog";
+import About from "../pages/About";
+import NotFound from "../pages/NotFound";
 
 const AppRouter = () => (
-    <Routes>
-        <Route path="/" element={<Blog />}></Route>
-        <Route path="/about" element={<About />}></Route>
-    </Routes>
-)
+  <Routes>
+    <Route path="/" element={<Blog />} />
+    <Route path="/about" element={<About />} />
+    <Route path="*" element={<NotFound />} />
+  </Routes>
+);
 
-export default AppRouter
+export default AppRouter;
