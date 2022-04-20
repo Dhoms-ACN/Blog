690f779ac4d6bcda0019406452d0d0fc68da3002
Pelago <domingo.pelago@accenture.com>
2022-04-20 10:50:18 +0800

    Add manual validation

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -3,68 +3,118 @@ import { Link } from "react-router-dom";
 import axios from "../utils/axios";
 
 const Register = () => {
-  const fields = {
+  const initialValues = {
       first_name: '',
       last_name: '',
       email: '',
-      password: ''
+      password: '',
+      confirm_password: ''
   }
 
-  const [data, setData] = useState(fields)
+  const [formData, setFormData] = useState(initialValues)
+  const [formErrors, setFormErrors] = useState({})
+
+
   const registerUser = (e) => {
     e.preventDefault()
-    axios.post('/sign-in', data).then(response => console.log(response.data))
-    .catch(e => console.log(e))
+    setFormErrors(validateForm(formData))
+    if(Object.keys(formErrors).length === 0) {
+      console.log('Submitting')
+    }
   }
+
+  const handleChange = (e) => {
+    const { value, name } = e.target
+    setFormData({ ...formData, [name]: value })
+  }
+
+  const validateForm = (data) => {
+    const errors = {}
+    if (!data.first_name) {
+      errors.first_name = "First name is required"
+    }
+    if (!data.last_name) {
+      errors.last_name = "Last name is required"
+    }
+    if (!data.email) {
+      errors.email = "Email is required"
+    }
+    if (!data.password) {
+      errors.password = "Password is required"
+    }
+    if(data.password !== data.confirm_password) {
+      errors.password = "Password did not match"
+    }
+
+    return errors
+  }
+
   return (
     <div className="min-h-screen flex flex-col">
       <div className="flex flex-1 items-center justify-center">
         <div className="rounded-lg sm:border-2 px-4 lg:px-24 py-16 lg:max-w-xl sm:max-w-md w-full text-center">
-          <form className="text-center">
+          <form className="text-center" onSubmit={registerUser}>
             <h1 className="font-bold tracking-wider text-3xl mb-8 w-full text-gray-600">
               Register
             </h1>
             <div className="py-2 text-left">
               <input
                 type="text"
+                name="first_name"
                 className="bg-gray-200 border-2 border-gray-100 focus:outline-none bg-gray-100 block w-full py-2 px-4 rounded-lg focus:border-gray-700 "
                 placeholder="First Name"
-                value={data.first_name}
-                onChange={(e) => setData({...data, first_name: e.target.value})}
+                value={formData.first_name}
+                onChange={handleChange}
               />
+              {formErrors.first_name && <span className="text-red-400 text-sm ml-2">{formErrors.first_name}</span>}
             </div>
             <div className="py-2 text-left">
               <input
                 type="text"
                 className="bg-gray-200 border-2 border-gray-100 focus:outline-none bg-gray-100 block w-full py-2 px-4 rounded-lg focus:border-gray-700 "
                 placeholder="Last Name"
-                value={data.last_name}
-                onChange={(e) => setData({...data, last_name: e.target.value})}
+                name="last_name"
+                value={formData.last_name}
+                onChange={handleChange}
               />
+              {formErrors.last_name && <span className="text-red-400 text-sm ml-2">{formErrors.last_name}</span>}
             </div>
             <div className="py-2 text-left">
               <input
                 type="email"
                 className="bg-gray-200 border-2 border-gray-100 focus:outline-none bg-gray-100 block w-full py-2 px-4 rounded-lg focus:border-gray-700 "
                 placeholder="Email"
-                value={data.email}
-                onChange={(e) => setData({...data, email: e.target.value})}
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
               />
+               {formErrors.email && <span className="text-red-400 text-sm ml-2">{formErrors.email}</span>}
             </div>
             <div className="py-2 text-left">
               <input
                 type="password"
                 className="bg-gray-200 border-2 border-gray-100 focus:outline-none bg-gray-100 block w-full py-2 px-4 rounded-lg focus:border-gray-700 "
                 placeholder="Password"
-                value={data.password}
-                onChange={(e) => setData({...data, password: e.target.value})}
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+              />
+               {formErrors.password && <span className="text-red-400 text-sm ml-2">{formErrors.password}</span>}
+            </div>
+            <div className="py-2 text-left">
+              <input
+                type="password"
+                className="bg-gray-200 border-2 border-gray-100 focus:outline-none bg-gray-100 block w-full py-2 px-4 rounded-lg focus:border-gray-700 "
+                placeholder="Confirm Password"
+                name="confirm_password"
+                value={formData.confirm_password}
+                onChange={handleChange}
               />
             </div>
             <div className="py-2">
               <button
                 type="submit"
                 className="border-2 border-gray-100 focus:outline-none bg-purple-600 text-white font-bold tracking-wider block w-full p-2 rounded-lg focus:border-gray-700 hover:bg-purple-700"
-                onClick={registerUser}
               >
                 Sign In
               </button>
